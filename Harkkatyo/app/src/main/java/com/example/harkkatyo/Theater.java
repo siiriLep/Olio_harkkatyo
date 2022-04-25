291acee9667f5242abae79d1cd98c409fdc1e2ea
Roope1 <roope70@live.com>
2022-04-26 00:20:56 +0300

    Theater manager and Theater cores done

--- Harkkatyo/app/src/main/java/com/example/harkkatyo/Theater.java
+++ Harkkatyo/app/src/main/java/com/example/harkkatyo/Theater.java
@@ -1,6 +1,19 @@
 package com.example.harkkatyo;
 
 
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
 /*
     ### Theater ###
     * Theater name and ID
@@ -12,11 +25,50 @@ package com.example.harkkatyo;
 public class Theater {
     private int id;
     private String name;
+    ArrayList<Movie> movies;
 
 
     Theater(int id, String name){
         this.id = id;
         this.name = name;
+    }
+
+    // Gets all the movies shown in the theater
+    void fetchMovies(String date){
+        movies = new ArrayList<>();
+        String url = "https://www.finnkino.fi/xml/Schedule/?area=" + id + "&dt=" + date;
+
+        try { // reading the xml
+            DocumentBuilder builder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+            Document doc = builder.parse(url);
+            doc.getDocumentElement().normalize();
+            NodeList nodes = doc.getDocumentElement().getElementsByTagName("Show");
+
+            for (int i = 0; i < nodes.getLength(); i++) {
+                Node node = nodes.item(i);
+                if(node.getNodeType() == Node.ELEMENT_NODE){
+                    Element element = (Element) node;
+
+                    Movie movie = new Movie(Integer.parseInt(element.getElementsByTagName("ID").item(0).getTextContent()),
+                            element.getElementsByTagName("Title").item(0).getTextContent());
+                            // MORE INFORMATION TO BE ADDED ONCE WE DECIDE SOMETHING
+                }
+            }
+        } catch (ParserConfigurationException | IOException | SAXException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public ArrayList<String> getMovieNames() {
+        ArrayList<String> movieNames = new ArrayList<>();
+        for (Movie movie : movies) {
+            movieNames.add(movie.getName());
+        }
+        return movieNames;
+    }
+
+    public Movie getMovie(int index) {
+        return movies.get(index);
     }
 
     public int getId() {
--- Harkkatyo/app/src/main/java/com/example/harkkatyo/TheaterManager.java
+++ Harkkatyo/app/src/main/java/com/example/harkkatyo/TheaterManager.java
@@ -1,5 +1,18 @@
 package com.example.harkkatyo;
 
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
 /*
    ### Manages all the theaters ###
    * Gets all theaters
@@ -11,5 +24,68 @@ package com.example.harkkatyo;
 */
 public class TheaterManager {
 
+    //General variables for this class
+    private static TheaterManager tm;
+    ArrayList<Theater> theaters;
+
+
+    // Singleton model
+    public static TheaterManager getInstance(){
+        if(tm == null){
+            tm = new TheaterManager();
+        }
+        return tm;
+    }
+
+    // Fetches all of the finnkino theaters from the xml file
+    public void fetchTheaters(){
+        theaters = new ArrayList<>();
+        try {
+            // read the XML
+            DocumentBuilder builder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+            String theatersXML = "https://www.finnkino.fi/xml/TheatreAreas/";
+            Document doc = builder.parse(theatersXML);
+            doc.getDocumentElement().normalize();
+            NodeList nodes = doc.getDocumentElement().getElementsByTagName("TheatreArea");
+
+
+            // foreach theater in XML create a theater object with the data given
+            for(int i = 0; i < nodes.getLength(); i++) {
+                Node node = nodes.item(i);
+
+                if (node.getNodeType() == Node.ELEMENT_NODE){
+                    Element element = (Element) node;
+
+                    // creating the actual object
+                    Theater theater = new Theater(Integer.parseInt(element.getElementsByTagName("ID").item(0).getTextContent()),
+                            element.getElementsByTagName("Name").item(0).getTextContent());
+
+                    // adding object to list of theaters
+                    theaters.add(theater);
+
+                }
+            }
+        } catch (ParserConfigurationException | IOException | SAXException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    // returns a list of theater names
+    public ArrayList<String> getTheaterNames() {
+        ArrayList<String> names = new ArrayList<>();
+        for(Theater theater : theaters) {
+            names.add(theater.getName());
+        }
+        return names;
+    }
+
+    // returns a single theater object
+    public Theater getTheater(int index){
+        return theaters.get(index);
+    }
+
 
 }
+
+

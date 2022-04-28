414b35f8c0e596fc088f0a6b7a2f2a767a0b409f
Roope1 <roope70@live.com>
2022-04-28 21:08:14 +0300

    Spinner initialized

--- Harkkatyo/app/src/main/java/com/example/harkkatyo/MainActivity.java
+++ Harkkatyo/app/src/main/java/com/example/harkkatyo/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.harkkatyo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.StrictMode;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
 
@@ -10,12 +12,23 @@ public class MainActivity extends AppCompatActivity {
 
     Spinner spinner;
     Button button;
+    TheaterManager manager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+
         spinner = findViewById(R.id.theaterNames);
         button = findViewById(R.id.button);
+        manager = TheaterManager.getInstance();
+
+        manager.fetchTheaters();
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, manager.getTheaterNames());
+        spinner.setAdapter(adapter);
     }
+
 }

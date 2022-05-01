75a0f9dcd5f6dab6c261c75e27d2de2cda7d0fcd
Roope1 <roope70@live.com>
2022-05-01 15:16:13 +0300

    UI upgrades

--- Harkkatyo/app/src/main/java/com/example/harkkatyo/TheaterActivity.java
+++ Harkkatyo/app/src/main/java/com/example/harkkatyo/TheaterActivity.java
@@ -3,18 +3,23 @@ package com.example.harkkatyo;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.TimePickerDialog;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.TimePicker;
 
 import java.time.LocalDate;
+import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 import java.time.format.FormatStyle;
 import java.util.ArrayList;
+import java.util.Locale;
 
 public class TheaterActivity extends AppCompatActivity {
 
@@ -22,6 +27,8 @@ public class TheaterActivity extends AppCompatActivity {
     ListView list;
     LocalDate date;
     TextView currentDateText;
+    LocalTime filterStart, filterEnd;
+    Button timePickerStart, timePickerEnd;
 
     @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
@@ -34,7 +41,11 @@ public class TheaterActivity extends AppCompatActivity {
         list = findViewById(R.id.list);
         date = LocalDate.now();
         currentDateText = findViewById(R.id.textView2);
+        timePickerStart = findViewById(R.id.StartTimePicker);
+        timePickerEnd = findViewById(R.id.EndTimePicker);
 
+        filterStart = LocalTime.MIN;
+        filterEnd = LocalTime.MAX;
 
         updateMovieList(date);
         updateDate(date);
@@ -64,5 +75,35 @@ public class TheaterActivity extends AppCompatActivity {
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, theater.getMovieNames());
         list.setAdapter(adapter);
         adapter.notifyDataSetChanged();
+    }
+
+    public void openStartTimePicker(View V){
+        TimePickerDialog.OnTimeSetListener onTimeSetListener = new TimePickerDialog.OnTimeSetListener() {
+            @Override
+            public void onTimeSet(TimePicker timePicker, int selectedHour, int selectedMinute) {
+                if(LocalTime.parse(String.format(Locale.getDefault(), "%02d:%02d", selectedHour, selectedMinute)).isBefore(filterEnd)) {
+                    filterStart = LocalTime.parse(String.format(Locale.getDefault(), "%02d:%02d", selectedHour, selectedMinute));
+                    timePickerStart.setText(String.format(Locale.getDefault(), "%02d:%02d", selectedHour, selectedMinute));
+                }
+            }
+        };
+        TimePickerDialog timePickerDialog = new TimePickerDialog(this, onTimeSetListener,00,00,true);
+        timePickerDialog.setTitle("Select start time");
+        timePickerDialog.show();
+    }
+
+    public void openEndTimePicker(View V){
+        TimePickerDialog.OnTimeSetListener onTimeSetListener = new TimePickerDialog.OnTimeSetListener() {
+            @Override
+            public void onTimeSet(TimePicker timePicker, int selectedHour, int selectedMinute) {
+                if(LocalTime.parse(String.format(Locale.getDefault(), "%02d:%02d", selectedHour, selectedMinute)).isAfter(filterStart)) {
+                    filterEnd = LocalTime.parse(String.format(Locale.getDefault(), "%02d:%02d", selectedHour, selectedMinute));
+                    timePickerEnd.setText(String.format(Locale.getDefault(), "%02d:%02d", selectedHour, selectedMinute));
+                }
+            }
+        };
+        TimePickerDialog timePickerDialog = new TimePickerDialog(this, onTimeSetListener,00,00,true);
+        timePickerDialog.setTitle("Select end time");
+        timePickerDialog.show();
     }
 }

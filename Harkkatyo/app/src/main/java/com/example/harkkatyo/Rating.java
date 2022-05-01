ab59f8320f9a808e6ab6dfc608f859bc4b8908cc
Roope1 <roope70@live.com>
2022-05-01 20:41:18 +0300

    ratings started

--- Harkkatyo/app/src/main/java/com/example/harkkatyo/Rating.java
+++ Harkkatyo/app/src/main/java/com/example/harkkatyo/Rating.java
@@ -6,14 +6,36 @@ package com.example.harkkatyo;
     * ratings from IMDB
     * ability to add own rating and comment
 
-
  */
 
-public class Rating {
-    private String movieName;
+import org.json.JSONException;
+import org.json.JSONObject;
 
-    Rating(String movieName){
-        this.movieName = movieName;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Rating {
+    private Movie movie;
+    private int id;
+    private String movieTitle;
+
+    Rating(Movie movie){
+        this.movie = movie;
+        this.id = movie.getId();
+        movieTitle = movie.getTitle();
+    }
+
+    public void addRating(int stars, String text) throws JSONException, IOException {
+        // Creating a json object
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("ID", id);
+        jsonObject.put("Title", movieTitle);
+        jsonObject.put("Stars", stars);
+        jsonObject.put("Comment", text);
+
+        FileWriter fw = new FileWriter("file:///android_asset/reviews/"+id+".json");
+        fw.write(String.valueOf(jsonObject));
+        fw.close();
     }
 
 

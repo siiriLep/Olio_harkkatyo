3273bac1c2ca4ef41187772aae5a3611e24cec7f
Roope1 <roope70@live.com>
2022-05-02 23:31:54 +0300

    Writing and reading JSON with GSON

--- Harkkatyo/app/src/main/java/com/example/harkkatyo/Rating.java
+++ Harkkatyo/app/src/main/java/com/example/harkkatyo/Rating.java
@@ -1,43 +1,32 @@
 package com.example.harkkatyo;
 
-
-/*
-    ### RATING ###
-    * ratings from IMDB
-    * ability to add own rating and comment
-
- */
-
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.io.FileWriter;
-import java.io.IOException;
-
 public class Rating {
-    private Movie movie;
+
+    private int stars;
+    private String comment;
     private int id;
-    private String movieTitle;
+    private String title;
 
-    Rating(Movie movie){
-        this.movie = movie;
-        this.id = movie.getId();
-        movieTitle = movie.getTitle();
+    Rating(int id, String title, int stars, String text){
+        this.stars = stars;
+        this.comment = text;
+        this.id = id;
+        this.title = title;
     }
 
-    public void addRating(int stars, String text) throws JSONException, IOException {
-        // Creating a json object
-        JSONObject jsonObject = new JSONObject();
-        jsonObject.put("ID", id);
-        jsonObject.put("Title", movieTitle);
-        jsonObject.put("Stars", stars);
-        jsonObject.put("Comment", text);
-
-        FileWriter fw = new FileWriter("file:///android_asset/reviews/"+id+".json");
-        fw.write(String.valueOf(jsonObject));
-        fw.close();
+    public int getStars() {
+        return stars;
     }
 
+    public String getComment() {
+        return comment;
+    }
 
+    public int getId() {
+        return id;
+    }
 
+    public String getTitle() {
+        return title;
+    }
 }
